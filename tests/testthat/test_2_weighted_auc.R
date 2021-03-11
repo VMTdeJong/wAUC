e334a0d30954fb15baf3c5e7cfc6d8e8fe808135
Valentijn de Jong <v.m.t.dejong-2@umcutrecht.nl>
2021-03-11 15:20:51 +0100

    Fixed typo in test for est_exact. Added exact tests for exact methods. Added est_exact_slow to microbenchmark (it is the slowest)

--- tests/testthat/test_2_weighted_auc.R
+++ tests/testthat/test_2_weighted_auc.R
@@ -12,36 +12,51 @@ y <- rbinom(n, 1, p)
 w <- runif(n, min = w_min, max = w_max)
 
 ### Tests
-test_that("For random weights, the exact and resampling method yield nearly equal point estimates", {
-  w_auc_est_exact     <- wAUC(y, p, w, method = "exact")
-  w_auc_est_replace   <- wAUC(y, p, w, method = "resampling", replace = TRUE)
-  w_auc_est_noreplace <- wAUC(y, p, w, method = "resampling", replace = FALSE)
-  
-  est_replace   <- w_auc_est_replace$estimate
-  est_noreplace <- w_auc_est_noreplace$estimate
-  est_exact     <- w_auc_est_replace$estimate
- 
+test_that("For random weights, the exact and resampling methods yield nearly equal point estimates", {
+  w_auc_est_exact      <- wAUC(y, p, w, method = "exact")
+  w_auc_est_replace    <- wAUC(y, p, w, method = "resampling", replace = TRUE)
+  w_auc_est_noreplace  <- wAUC(y, p, w, method = "resampling", replace = FALSE)
+
+  est_replace    <- w_auc_est_replace$estimate
+  est_noreplace  <- w_auc_est_noreplace$estimate
+  est_exact      <- w_auc_est_exact$estimate
+
   expect_lt(est_replace - est_noreplace, .005)
   expect_lt(est_noreplace - est_exact  , .005)
   expect_lt(est_exact - est_replace    , .005)
+  
+  
+})
+
+test_that("For random weights, the two exact methods yield identical point estimates", {
+  w_auc_est_exact      <- wAUC(y, p, w, method = "exact")
+  w_auc_est_exact_slow <- wAUC_exact_slow(y, p, w)
+  
+  est_exact      <- w_auc_est_exact$estimate
+  est_exact_slow <- w_auc_est_exact_slow$estimate
+  
+  expect_identical(est_exact, est_exact_slow)
 })
 
 
-# library("microbenchmark")
-# 
-# n <- 100
-# p <- runif(n)
-# y <- rbinom(n, 1, p)
-# w <- runif(n, w_min, w_max)
-# I <- 250
-# 
-# microbenchmark(AUC_default(y, p), 
-#                AUC_factorial(y, p),
-#                wAUC_exact(y, p, w),
-#                wAUC(y, p, w, method = "exact"))
-# 
-# microbenchmark(wAUC(y, p, w, method = "exact"),
-#                wAUC(y, p, w, method = "resampling", I = I, replace = TRUE),
-#                wAUC(y, p, w, method = "resampling", I = I, replace = FALSE),
-#                wAUC(y, p, w, method = "resampling", I = I, replace = TRUE,  AUC_method = "AUC_factorial"),
-#                wAUC(y, p, w, method = "resampling", I = I, replace = FALSE, AUC_method = "AUC_factorial"))
+
+
+library("microbenchmark")
+
+n <- 100
+p <- runif(n)
+y <- rbinom(n, 1, p)
+w <- runif(n, w_min, w_max)
+I <- 250
+
+microbenchmark(AUC_default(y, p),
+               AUC_factorial(y, p),
+               wAUC_exact(y, p, w),
+               wAUC(y, p, w, method = "exact"),
+               wAUC_exact_slow(y, p, w))
+
+microbenchmark(wAUC(y, p, w, method = "exact"),
+               wAUC(y, p, w, method = "resampling", I = I, replace = TRUE),
+               wAUC(y, p, w, method = "resampling", I = I, replace = FALSE),
+               wAUC(y, p, w, method = "resampling", I = I, replace = TRUE,  AUC_method = "AUC_factorial"),
+               wAUC(y, p, w, method = "resampling", I = I, replace = FALSE, AUC_method = "AUC_factorial"))
